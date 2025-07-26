a2610b5f1e71d859c072a2ff0179a3de6e2ed912
Pragat Pandya <jarvis@Pragats-MacBook-Air.local>
2025-07-27 05:20:34 +0530

    Define the index route.

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,17 @@ import express from 'express';
 // Initialze the server express
 const app = express();
 
+
+
+// route, controller function
+app.get("/", (req, res) => {    
+    res.send("Server is up and running!")
+});
+
+
+
+
+// Server listening on localhost 5001 port
 app.listen(5001, () => {
     console.log("Server Started at http://localhost:5001");
 });
