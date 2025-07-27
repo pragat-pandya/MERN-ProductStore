6a3fa12d4c1b1cede1de4da74a8547fded69bcc4
Pragat Pandya <jarvis@Pragats-MacBook-Air.local>
2025-07-27 07:50:27 +0530

    Add remaining CRUD endpoints for product resource.

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from "./config/db.js";
 import Product from './models/product.model.js';
+import mongoose, { mongo } from 'mongoose';
 
 
 // Enables access to env vars.
@@ -48,6 +49,73 @@ app.post('/api/products', async (req, res) => {
     }
 });
 
+app.delete('/api/products/:id', async(req, res) => {
+    const { id } = req.params;
+    console.log("ID:", id);
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({
+            success: false,
+            message: "Invalid Proudct ID"
+        });
+    }
+    try{
+        await Product.findByIdAndDelete(id);
+        return res.status(200).json({
+            success: true,
+            message: "Product deleted!"
+        });
+    } catch(error) {
+        console.log("Unable to delete the product: ", id);
+        return res.status(500).json({
+            success: false,
+            message: "Server Error!"
+        });
+    }
+});
+
+app.get('/api/products', async (req, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json({
+            success: true,
+            message: "All products",
+            data: products,
+        });
+    } catch (error) {
+        console.log("Error in fetching products!");
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+});
+
+app.put('/api/products/:id', async (req, res) => {
+    
+    const { id } = req.params;
+    const product = req.body;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({
+            success: false,
+            message: "Invalid Proudct ID"
+        });
+    }
+    try{
+        const updated = await Product.findByIdAndUpdate(id, product, {new:true});
+
+        res.status(200).json({
+            success:true,
+            message: "Updated successfully!"
+        });
+        // By default old object is returned with new true the updated object is returned.
+    } catch(error) {
+        console.log("ERROR:", error.message);
+        res.status(500).json({
+            success: false,
+            message: "SERVER ERROR!"
+        });
+    }
+});
 
 // Server listening on localhost 5001 port
 app.listen(5001, () => {
