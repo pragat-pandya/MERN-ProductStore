1b6cc490c5a0b22598f14b87909dde77451fb456
Pragat Pandya <jarvis@Pragats-MacBook-Air.local>
2025-07-27 05:56:27 +0530

    Add mongo connection on server start.

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,27 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from "./config/db.js";
+
+
+// Enables access to env vars.
+dotenv.config();
 
 // Initialze the server express
 const app = express();
-
-
 
 // route, controller function
 app.get("/", (req, res) => {    
     res.send("Server is up and running!")
 });
 
-
+// Define products route
+app.get("/products", (req, res) => {
+    res.send("products")
+});
 
 
 // Server listening on localhost 5001 port
 app.listen(5001, () => {
+    connectDB();
     console.log("Server Started at http://localhost:5001");
 });
-
