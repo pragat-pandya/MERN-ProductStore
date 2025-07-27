198018a56d9c818a99c451990189ad4e729692d3
Pragat Pandya <jarvis@Pragats-MacBook-Air.local>
2025-07-27 06:17:10 +0530

    Add products post URL for new product creation.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from "./config/db.js";
+import Product from './models/product.model.js';
 
 
 // Enables access to env vars.
@@ -8,6 +9,10 @@ dotenv.config();
 
 // Initialze the server express
 const app = express();
+// Allows to accept json data in the request.body. |MIDDELWARE FROM expres|
+app.use(express.json());
+
+
 
 // route, controller function
 app.get("/", (req, res) => {    
@@ -15,8 +20,32 @@ app.get("/", (req, res) => {
 });
 
 // Define products route
-app.get("/products", (req, res) => {
-    res.send("products")
+app.post('/api/products', async (req, res) => {
+    // user sent data
+    const product = req.body;
+
+    if (!product.name || !product.price || !product.image) {
+        return res.status(400).json({
+            success: false,
+            message: "Please provide all the fields"
+        });
+    } else {
+        const newProduct = new Product(product);
+        try {
+            await newProduct.save();
+            return res.status(201).json({
+                success: true,
+                message: "New product created successfully",
+                data: newProduct
+            });
+        } catch (error) {
+            console.error("Error in create product: ", error.message);
+            return res.status(500).json({
+                success: false,
+                message: error.message
+            });
+        }
+    }
 });
 
 
