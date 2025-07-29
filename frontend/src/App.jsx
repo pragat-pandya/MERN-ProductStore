0e1316302741584a19ec9510c18579ddba9006da
Pragat Pandya <jarvis@Pragats-MacBook-Air.local>
2025-07-29 23:44:49 +0530

    Define homePage and CreatePage routes and placeholder components.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,24 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { Button } from '@chakra-ui/react'
-
+import { Box, Button } from '@chakra-ui/react'
+import {Route, Routes} from "react-router-dom"
+import {HomePage} from './pages/HomePage.jsx'
+import {CreatePage} from './pages/CreatePage.jsx'
+import {Navbar} from './components/Navbar.jsx'
+ 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Button>
-        Hello 
-      </Button>
+      <Box minH={'100vh'}>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />}></Route>
+          <Route path="/create" element={<CreatePage />}></Route>
+        </Routes>
+      </Box>
     </>
   )
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import {ChakraProvider } from "@chakra-ui/react"
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ChakraProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ChakraProvider>
   </StrictMode>,
 )
